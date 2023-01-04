541efd89f302cbecae83150d603d0cbf69fbe923
wintermute <justinreeves.tch@gmail.com>
2023-01-03 23:25:32 -0500

    wip

--- terraform/foundry/iam.tf
+++ terraform/foundry/iam.tf
@@ -36,27 +36,27 @@ resource "aws_iam_policy" "foundry_s3_access" {
   policy = data.aws_iam_policy_document.foundry_s3_access.json
 }
 
-data "aws_iam_policy_document" "assume_role" {
+data "aws_iam_policy_document" "ecs_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
+      identifiers = ["ecs.amazonaws.com"]
     }
   }
 }
 
 resource "aws_iam_role" "foundry_s3_access" {
   name               = "foundry-s3-access"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name               = "ecsTaskExecutionRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.task_assume_role_policy.json
 }
 
-data "aws_iam_policy_document" "assume_role_policy" {
+data "aws_iam_policy_document" "task_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
 
